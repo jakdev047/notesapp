631436440f3af0512f512762b3000936da66a36c
jakdev047 <shuvo047niter@gmail.com>
2020-04-30 23:23:57 +0600

    single note route demo

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,67 @@
-// require files
+/* ===============
+    Require Files
+  ================
+*/
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-// port
+/* ============
+    Port
+  =============
+*/
 const PORT = process.env.PORT || 8080;
 
-// middleweare
+/* ===============
+    Middleweare
+  ================
+*/
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// route
+/* ============
+    Route
+  =============
+*/
+
+// home route
 app.get('/',(req,res)=>{
   res.json({
     message: `Welcome My Notes App`
   });
 });
 
+const notes = [
+  {id:1,title:'Note One',comment: 'This First Note'},
+  {id:2,title:'Note Two',comment: 'This Second Note'}
+]
+
+// notes route
+app.get('/notes',(req,res)=>{
+  res.json(notes);
+});
+
+// single note route
+app.get('/notes/:id',(req,res)=>{
+  const id = req.params.id;
+  const note = notes.filter(item => item.id === parseInt(id));
+  res.json(note);
+});
+
+// not found route
 app.get('*',(req,res)=> {
   res.json({
-    error: '404 !!!'
+    error: '404 Not Found'
   })
 })
 
-// listen
+/* ============
+    Listen
+  =============
+*/
 app.listen(PORT,()=>{
   console.log(`${PORT} Server is Running...`)
 });
