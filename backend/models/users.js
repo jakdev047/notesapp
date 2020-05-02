3cd0f760248a6339d862c8a5ec1cce60d3c6e7bf
jakdev047 <shuvo047niter@gmail.com>
2020-05-02 16:36:13 +0600

    user schema create

---
+++ backend/models/users.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const validator = require('validator'); 
+
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema(
+  {
+    firstName:  { 
+      type: String,
+      required: [true,'First Name is Required'] 
+    },
+    lastName:  { 
+      type: String,
+      required: [true,'Last Name is Required'] 
+    },
+    email:  { 
+      type: String,
+      unique:true,
+      trim:true,
+      required: [true,'Email is Required'],
+      validate: {
+        validator(value) {
+          return validator.isEmail(value)
+        },
+        message: " Must be a valid Email"
+      } 
+    },
+    password:  { 
+      type: String,
+      minlength:[6,'Password must be more than 6 character'],
+      required: [true,'Password is Required'],
+      validate:{
+        validator(value){
+          return value.toLowercase().includes('password');
+        },
+        message: " Password must not contain 'password' "
+      }
+    }
+  },
+  {timestamps: true}
+);
+
+const User = mongoose.model('User', usersSchema);
+
+module.exports = User;
