e0ee1d341898aea42efca2cca60a2d6a6392a500
jakdev047 <shuvo047niter@gmail.com>
2020-05-02 15:18:38 +0600

    to create controllers/notesController folder

---
+++ backend/controllers/notesControllers.js
@@ -0,0 +1,112 @@
+const { validationResult } = require('express-validator');
+const Note = require('../models/notes');
+
+module.exports.addNoteController = async(req,res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  try {
+    const newNote = new Note(req.body);
+    await newNote.save();
+    res.status(200).json(newNote);
+  } 
+  catch (err) {
+    res.status(500).send(err)
+  }
+};
+
+module.exports.getAllNotesController = async(req,res)=>{
+  try {
+    const notes = await Note.find(); // all notes collections
+    if( notes.length == 0) {
+      return res.status(404).json({
+        msg: 'No Notes Found or Not Yet Created'
+      });
+    }
+    else {
+      return res.status(200).json(notes);
+    }
+  } 
+  catch (error) {
+    res.status(500).send(err)
+  }
+};
+
+module.exports.getSingleNoteController = async(req,res)=>{
+  const id = req.params.id;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(404).json({ errors: errors.array() });
+  }
+  try {
+    const note = await Note.findById(id);
+    if ( note) {
+      return res.status(200).json(note);
+    }
+    else {
+      res.status(404).json({
+        msg: 'Notes Not Found'
+      });
+    }
+  } 
+  catch (error) {
+    res.status(500).send(err)
+  }
+};
+
+module.exports.updateNoteController = async(req,res)=>{
+
+  const updateNoteId = req.params.id; // id
+
+  const gotUserInput = Object.keys(req.body); // id, title, comment
+  const allowUpdates = ['title','comment']; // title, comment
+  const isAllowed = gotUserInput.every( update => allowUpdates.includes(update)); // false
+
+  if(!isAllowed) {
+    return res.status(400).json({
+      msg: 'Invalid Updates'
+    })
+  }
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(404).json({ errors: errors.array() });
+  }
+
+  try {
+    const updateNote = await Note.findByIdAndUpdate(updateNoteId,req.body,{
+      new:true,
+      runValidators:true
+    });
+
+    if(!updateNote) return res.status(404).send('Note Not Found');
+    res.status(200).send(updateNote);
+    
+  } 
+  catch (error) {
+    res.status(500).send(err)
+  }
+}
+
+module.exports.deleteNoteController = async(req,res)=>{
+  const deleteId = req.params.id;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(404).json({ errors: errors.array() });
+  }
+  try {
+    const deleteNote = await Note.findByIdAndDelete(deleteId);
+    if(deleteNote){
+      return res.status(200).json(notes);
+    }
+    else{
+      return res.status(404).json({
+        msg: 'Not Not Found!!!'
+      });
+    }
+  } 
+  catch (error) {
+    return res.status(500).json('Internal Server Error')
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 /* =============== Require Files ================ */
 const express = require('express');
 const mongoose = require('mongoose');
-const { check, validationResult } = require('express-validator');
+const { check,validationResult} = require('express-validator');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -12,6 +12,9 @@ const PORT = process.env.PORT || 8080;
 
 /* ============ Model============= */
 const Note = require('./models/notes');
+
+/* ============ Controller ============= */
+const {addNoteController,getAllNotesController,getSingleNoteController,updateNoteController,deleteNoteController} = require('./controllers/notesControllers');
 
 /* ============ Connecting mongodb ============= */
 mongoose
@@ -38,126 +41,23 @@ app.get('/',(req,res)=>{
 app.post('/notes',[
     check('title').notEmpty().isLength({min:3,max:50}).withMessage('Titile is Required & must 3 to 50 charecter'),
     check('comment').notEmpty().isLength({min:5,max:500}).withMessage('Comment is Required & must 5 to 500 charecter')
-  ],
-  async(req,res)=> {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
-    try {
-      const newNote = new Note(req.body);
-      await newNote.save();
-      res.status(200).json(newNote);
-      // notes = [...notes,newNote];
-      // res.status(202).json(notes);
-    } 
-    catch (err) {
-      res.status(500).send(err)
-    }
-});
+  ],addNoteController);
 
 // notes route
-app.get('/notes',async(req,res)=>{
-  try {
-    const notes = await Note.find(); // all notes collections
-    if( notes.length == 0) {
-      return res.status(404).json({
-        msg: 'No Notes Found or Not Yet Created'
-      });
-    }
-    else {
-      return res.status(200).json(notes);
-    }
-  } 
-  catch (error) {
-    res.status(500).send(err)
-  }
-});
+app.get('/notes',getAllNotesController);
 
 // single note route
-app.get('/notes/:id',check('id','Note not Found').isMongoId(),async(req,res)=>{
-  const id = req.params.id;
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(404).json({ errors: errors.array() });
-  }
-  try {
-    const note = await Note.findById(id);
-    if ( note) {
-      return res.status(200).json(note);
-    }
-    else {
-      res.status(404).json({
-        msg: 'Notes Not Found'
-      });
-    }
-  } 
-  catch (error) {
-    res.status(500).send(err)
-  }
-});
+app.get('/notes/:id',check('id','Note not Found').isMongoId(),getSingleNoteController);
 
 // update note
 app.put('/notes/:id',[
   check('id','Note No Found').isMongoId(),
   check('title','title is required').optional().notEmpty(),
   check('comment','comment is required').optional().notEmpty(),
-  ],async(req,res)=>{
-
-  const updateNoteId = req.params.id; // id
-
-  const gotUserInput = Object.keys(req.body); // id, title, comment
-  const allowUpdates = ['title','comment']; // title, comment
-  const isAllowed = gotUserInput.every( update => allowUpdates.includes(update)); // false
-
-  if(!isAllowed) {
-    return res.status(400).json({
-      msg: 'Invalid Updates'
-    })
-  }
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(404).json({ errors: errors.array() });
-  }
-
-  try {
-    const updateNote = await Note.findByIdAndUpdate(updateNoteId,req.body,{
-      new:true,
-      runValidators:true
-    });
-
-    if(!updateNote) return res.status(404).send('Note Not Found');
-    res.status(200).send(updateNote);
-    
-  } 
-  catch (error) {
-    res.status(500).send(err)
-  }
-})
+  ],updateNoteController)
 
 // delete note
-app.delete('/notes/:id',check('id','Note Not Found').isMongoId(),async(req,res)=>{
-  const deleteId = req.params.id;
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(404).json({ errors: errors.array() });
-  }
-  try {
-    const deleteNote = await Note.findByIdAndDelete(deleteId);
-    if(deleteNote){
-      return res.status(200).json(notes);
-    }
-    else{
-      return res.status(404).json({
-        msg: 'Not Not Found!!!'
-      });
-    }
-  } 
-  catch (error) {
-    return res.status(500).json('Internal Server Error')
-  }
-})
+app.delete('/notes/:id',check('id','Note Not Found').isMongoId(),deleteNoteController)
 
 // not found route
 app.get('*',(req,res)=> {
