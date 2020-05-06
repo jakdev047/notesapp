8c4f727b15c99303bda57b9d4fb917a3493a346d
jakdev047 <shuvo047niter@gmail.com>
2020-05-06 16:53:37 +0600

    use remote database

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 module.exports.connectDB = async() => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/notesapp', {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false})
+    await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@dbh10.mlab.com:27107/notesapp`, 
+    {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false})
     console.log('DataBase Connected Succesfully');
   } 
   catch (error) {
